76ae0c468dfbf3913c7d0b6355df5ab6af9996d1
Alex Carreira <aycarrei@gmail.com>
2016-10-15 17:01:21 -0700

    Changes to get RNBranch (react-native-branch) to work (fixing undefined issue on RNBranch.setDebug call).

--- android/app/src/main/java/com/inphoodrn/MainActivity.java
+++ android/app/src/main/java/com/inphoodrn/MainActivity.java
@@ -12,8 +12,8 @@ import com.facebook.reactnative.androidsdk.FBSDKPackage;
 //import com.lwansbrough.ReactCamera.ReactCamera;
 
 // From: https://dev.branch.io/getting-started/sdk-integration-guide/guide/react/#android-configure-manifest
-import android.content.Intent;
 import io.branch.rnbranch.*;
+import android.content.Intent;
 
 // Needed to do this to get compilation/deployment to work (comment out ReactCamera)--remains to be
 // seen if the component will actually work
@@ -34,14 +34,6 @@ public class MainActivity extends ReactActivity {
     protected void onStart() {
         super.onStart();
         RNBranchModule.initSession(this.getIntent().getData(), this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // The following is recommended, but onStop is not defined in RNBranchModule, commenting out
-        // for now.  (See: https://dev.branch.io/getting-started/sdk-integration-guide/guide/react/#start-a-branch-session)
-//        RNBranchModule.onStop();
     }
 
     @Override
--- android/app/src/main/java/com/inphoodrn/MainApplication.java
+++ android/app/src/main/java/com/inphoodrn/MainApplication.java
@@ -12,6 +12,11 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+// import Branch and RNBRANCH
+import io.branch.rnbranch.*;
+import io.branch.referral.Branch;
+
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -23,10 +28,17 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNBranchPackage()
       );
     }
   };
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Branch.getAutoInstance(this);
+  }
 
   @Override
   public ReactNativeHost getReactNativeHost() {

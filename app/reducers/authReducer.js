4355d70c07f1dc104603c11d0e932c3def783995
Alex Carreira <aycarrei@gmail.com>
2016-11-03 21:09:11 -0700

    Add action. to error to get rid of red screen.

--- app/reducers/authReducer.js
+++ app/reducers/authReducer.js
@@ -41,7 +41,7 @@ export default function authentication(state = initialState, action) {
       return {
         ...initialState,
         value: null,
-        error: error
+        error: action.error
       }
     case LOGOUT_ERROR:
       return {

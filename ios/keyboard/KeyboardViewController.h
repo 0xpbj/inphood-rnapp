8cc629108225297acfe5e87031c73bdb6495acb0
inphood <prabhaav.bhardwaj@gmail.com>
2016-12-05 13:27:03 -0800

    fix for sagas crash

---
+++ ios/keyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  keyboard
+//
+//  Created by Prabhaav Bhardwaj on 12/2/16.
+//  Copyright © 2016 inPhood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
